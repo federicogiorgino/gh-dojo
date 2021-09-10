7e12ef9cf72bffd8c36cd5ec139ea97ddb28ec86
Federico Giorgino <f_giorgino@icloud.com>
2021-09-10 19:29:28 +0200

    Cleanup + Routing Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router";
+import Homepage from "./pages/Homepage";
+import Userpage from "./pages/Userpage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Homepage} />
+      <Route path="/user/:id" component={Userpage} />
+    </Switch>
   );
-}
+};
 
 export default App;
