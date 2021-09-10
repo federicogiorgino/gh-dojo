db943af9e0792fb242522439a1f9ce156c0c018c
Federico Giorgino <f_giorgino@icloud.com>
2021-09-10 19:58:20 +0200

    Homepage

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import "./bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, FC } from "react";
+import { History } from "history";
+import { Col, Container, Form, Row, Button } from "react-bootstrap";
+import homepageStyles from "./styles.module.css";
 
-const Homepage = () => {
-  return <div></div>;
+interface IProps {
+  history: History;
+}
+
+const Homepage: FC<IProps> = ({ history }) => {
+  const [username, setUsername] = useState("");
+  return (
+    <Container className="vh-100 flex-center">
+      <div className={`mb-3 ${homepageStyles.logoContainer}`}>
+        <img
+          src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+          alt="Logo"
+          className={homepageStyles.logo}
+        />
+      </div>
+      <h1 className="text-main text-xl-center">Git Dojo</h1>
+      <Form
+        className="flex-center"
+        onSubmit={(e) => {
+          e.preventDefault();
+          history.push(`/user/${username}`);
+        }}
+      >
+        <Row className="mt-3 mb-3">
+          <Col>
+            <Form.Control
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Col>
+        </Row>
+        <Row className="mt-3 mb-3">
+          <Col>
+            <Button variant="info" type="submit">
+              Search
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
+  );
 };
 
 export default Homepage;
